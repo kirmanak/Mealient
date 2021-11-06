8bae6205b945c80ffa09fbe35e2008952ae10ba2
Kirill Kamakin <k.kamakin@protonmail.ch>
2021-11-06 22:28:14 +0300

    Initial commit
    This commit contains Fragment + ViewModel template from Android Studio

---
+++ app/src/main/java/gq/kirmanak/mealie/MainActivity.kt
@@ -0,0 +1,18 @@
+package gq.kirmanak.mealie
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import gq.kirmanak.mealie.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/gq/kirmanak/mealie/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package gq.kirmanak.mealie.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
