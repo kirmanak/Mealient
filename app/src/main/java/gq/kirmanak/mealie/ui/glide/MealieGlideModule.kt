740dd12f3bd2d307c98f3385d0248a8c39d45c81
Kirill Kamakin <k.kamakin@protonmail.ch>
2021-11-13 13:42:40 +0000

    Enable lint rules and fix found issues

--- app/src/main/java/gq/kirmanak/mealie/ui/glide/MealieGlideModule.kt
+++ app/src/main/java/gq/kirmanak/mealie/ui/glide/MealieGlideModule.kt
@@ -4,6 +4,4 @@ import com.bumptech.glide.annotation.GlideModule
 import com.bumptech.glide.module.AppGlideModule
 
 @GlideModule
-class MealieGlideModule : AppGlideModule() {
-
-}
+class MealieGlideModule : AppGlideModule()
