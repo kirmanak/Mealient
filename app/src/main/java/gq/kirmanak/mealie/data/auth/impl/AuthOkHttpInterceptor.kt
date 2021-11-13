0ff9afa4c67413216062aec40af032b506ce1653
Kirill Kamakin <k.kamakin@protonmail.ch>
2021-11-13 19:03:13 +0300

    Add OkHttpBuilder tests

--- app/src/main/java/gq/kirmanak/mealie/data/auth/impl/AuthOkHttpInterceptor.kt
+++ app/src/main/java/gq/kirmanak/mealie/data/auth/impl/AuthOkHttpInterceptor.kt
@@ -3,13 +3,15 @@ package gq.kirmanak.mealie.data.auth.impl
 import okhttp3.Interceptor
 import okhttp3.Response
 
+const val AUTHORIZATION_HEADER = "Authorization"
+
 class AuthOkHttpInterceptor(token: String) : Interceptor {
     private val headerValue = "Bearer $token"
 
     override fun intercept(chain: Interceptor.Chain): Response {
         val newRequest = chain.request()
             .newBuilder()
-            .addHeader("Authorization", headerValue)
+            .addHeader(AUTHORIZATION_HEADER, headerValue)
             .build()
         return chain.proceed(newRequest)
     }
