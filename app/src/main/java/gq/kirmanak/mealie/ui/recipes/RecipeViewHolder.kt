9da3c44d401a58b73e6962a4e7b8cb9e417339c7
Kirill Kamakin <k.kamakin@protonmail.ch>
2021-11-14 19:11:02 +0300

    Support showing placeholders in recipe view holders

--- app/src/main/java/gq/kirmanak/mealie/ui/recipes/RecipeViewHolder.kt
+++ app/src/main/java/gq/kirmanak/mealie/ui/recipes/RecipeViewHolder.kt
@@ -1,6 +1,7 @@
 package gq.kirmanak.mealie.ui.recipes
 
 import androidx.recyclerview.widget.RecyclerView
+import gq.kirmanak.mealie.R
 import gq.kirmanak.mealie.data.recipes.db.RecipeEntity
 import gq.kirmanak.mealie.databinding.ViewHolderRecipeBinding
 
@@ -8,8 +9,12 @@ class RecipeViewHolder(
     private val binding: ViewHolderRecipeBinding,
     private val recipeViewModel: RecipeViewModel
 ) : RecyclerView.ViewHolder(binding.root) {
+    private val loadingPlaceholder by lazy {
+        binding.root.resources.getString(R.string.view_holder_recipe_text_placeholder)
+    }
+
     fun bind(item: RecipeEntity?) {
-        binding.name.text = item?.name
+        binding.name.text = item?.name ?: loadingPlaceholder
         recipeViewModel.loadRecipeImage(binding.image, item)
     }
 }
