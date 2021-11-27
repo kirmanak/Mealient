6841818214777eba04dec5aed1a899e5010e77ba
Kirill Kamakin <k.kamakin@protonmail.ch>
2021-11-27 15:22:36 +0300

    Fix systemUiVisibility deprecation warnings
    This commit extracts setSystemUiVisibility as
    an extension and uses the new API depending on
    the Build.VERSION

--- app/src/main/java/gq/kirmanak/mealient/ui/ViewExtensions.kt
+++ app/src/main/java/gq/kirmanak/mealient/ui/ViewExtensions.kt
@@ -1,5 +1,11 @@
 package gq.kirmanak.mealient.ui
 
+import android.app.Activity
+import android.os.Build
+import android.view.View
+import android.view.WindowInsets
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.asLiveData
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
@@ -28,4 +34,30 @@ fun SwipeRefreshLayout.refreshesLiveData(): LiveData<Unit> {
         }
     }
     return callbackFlow.asLiveData()
+}
+
+fun Activity.setSystemUiVisibility(isVisible: Boolean) {
+    Timber.v("setSystemUiVisibility() called with: isVisible = $isVisible")
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) setSystemUiVisibilityV30(isVisible)
+    else setSystemUiVisibilityV1(isVisible)
+}
+
+@Suppress("DEPRECATION")
+private fun Activity.setSystemUiVisibilityV1(isVisible: Boolean) {
+    Timber.v("setSystemUiVisibilityV1() called with: isVisible = $isVisible")
+    window.decorView.systemUiVisibility = if (isVisible) 0 else View.SYSTEM_UI_FLAG_FULLSCREEN
+}
+
+@RequiresApi(Build.VERSION_CODES.R)
+private fun Activity.setSystemUiVisibilityV30(isVisible: Boolean) {
+    Timber.v("setSystemUiVisibilityV30() called with: isVisible = $isVisible")
+    val systemBars = WindowInsets.Type.systemBars()
+    window.insetsController?.apply { if (isVisible) show(systemBars) else hide(systemBars) }
+        ?: Timber.w("setSystemUiVisibilityV30: insets controller is null")
+}
+
+fun AppCompatActivity.setActionBarVisibility(isVisible: Boolean) {
+    Timber.v("setActionBarVisibility() called with: isVisible = $isVisible")
+    supportActionBar?.apply { if (isVisible) show() else hide() }
+        ?: Timber.w("setActionBarVisibility: action bar is null")
 }
