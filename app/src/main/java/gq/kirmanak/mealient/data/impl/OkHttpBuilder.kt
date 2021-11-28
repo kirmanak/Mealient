25f14226df40d8289756b9e8caaeea237847db4e
Kirill Kamakin <k.kamakin@protonmail.ch>
2021-11-28 21:55:29 +0300

    Replace Stetho with Flipper, add LeakCanary

--- app/src/main/java/gq/kirmanak/mealient/data/impl/OkHttpBuilder.kt
+++ app/src/main/java/gq/kirmanak/mealient/data/impl/OkHttpBuilder.kt
@@ -1,27 +1,21 @@
 package gq.kirmanak.mealient.data.impl
 
-import gq.kirmanak.mealient.BuildConfig
-import gq.kirmanak.mealient.data.auth.impl.AuthOkHttpInterceptor
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 import timber.log.Timber
 import javax.inject.Inject
 
-class OkHttpBuilder @Inject constructor(
-    private val authOkHttpInterceptor: AuthOkHttpInterceptor
+class OkHttpBuilder
+@Inject
+constructor(
+    // Use @JvmSuppressWildcards because otherwise dagger can't inject it (https://stackoverflow.com/a/43149382)
+    private val interceptors: Set<@JvmSuppressWildcards Interceptor>
 ) {
+
     fun buildOkHttp(): OkHttpClient {
         Timber.v("buildOkHttp() called")
-        val builder = OkHttpClient.Builder()
-        if (BuildConfig.DEBUG) builder.addNetworkInterceptor(buildLoggingInterceptor())
-        builder.addNetworkInterceptor(authOkHttpInterceptor)
-        return builder.build()
-    }
-
-    private fun buildLoggingInterceptor(): Interceptor {
-        val interceptor = HttpLoggingInterceptor { message -> Timber.tag("OkHttp").v(message) }
-        interceptor.level = HttpLoggingInterceptor.Level.BODY
-        return interceptor
+        return OkHttpClient.Builder()
+            .apply { for (interceptor in interceptors) addNetworkInterceptor(interceptor) }
+            .build()
     }
 }
---
+++ app/src/release/java/gq/kirmanak/mealient/App.kt
@@ -0,0 +1,7 @@
+package gq.kirmanak.mealient
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App : Application()
