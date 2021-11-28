cc2fb772690ed896f9e86ec616f94815992111c7
Kirill Kamakin <k.kamakin@protonmail.ch>
2021-11-28 16:32:05 +0300

    Add Stetho to debug application (#18)

--- app/src/main/java/gq/kirmanak/mealient/App.kt
+++ app/src/main/java/gq/kirmanak/mealient/App.kt
@@ -1,6 +1,7 @@
 package gq.kirmanak.mealient
 
 import android.app.Application
+import com.facebook.stetho.Stetho
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
@@ -8,7 +9,10 @@ import timber.log.Timber
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
-        if (BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+            Stetho.initializeWithDefaults(this)
+        }
         Timber.v("onCreate() called")
     }
 }
