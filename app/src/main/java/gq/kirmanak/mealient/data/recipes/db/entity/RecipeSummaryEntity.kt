ef2aa5b412e18e9fdb38cc21e7c5f4660e198d4b
Kirill Kamakin <k.kamakin@protonmail.ch>
2022-04-04 02:37:51 +0500

    Allow recipe image to be null

--- app/src/main/java/gq/kirmanak/mealient/data/recipes/db/entity/RecipeSummaryEntity.kt
+++ app/src/main/java/gq/kirmanak/mealient/data/recipes/db/entity/RecipeSummaryEntity.kt
@@ -11,13 +11,13 @@ data class RecipeSummaryEntity(
     @PrimaryKey @ColumnInfo(name = "remote_id") val remoteId: Long,
     @ColumnInfo(name = "name") val name: String,
     @ColumnInfo(name = "slug") val slug: String,
-    @ColumnInfo(name = "image") val image: String,
+    @ColumnInfo(name = "image") val image: String?,
     @ColumnInfo(name = "description") val description: String,
     @ColumnInfo(name = "rating") val rating: Int?,
     @ColumnInfo(name = "date_added") val dateAdded: LocalDate,
     @ColumnInfo(name = "date_updated") val dateUpdated: LocalDateTime
 ) {
     override fun toString(): String {
-        return "RecipeEntity(remoteId=$remoteId, name='$name')"
+        return "RecipeSummaryEntity(remoteId=$remoteId, name='$name')"
     }
 }
