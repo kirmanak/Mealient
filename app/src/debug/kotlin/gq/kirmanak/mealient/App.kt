bca25ae0b319361689a05a84f0255740068a16f8
Kirill Kamakin <k.kamakin@protonmail.ch>
2022-06-03 18:20:35 +0200

    Remove Flipper

--- app/src/debug/kotlin/gq/kirmanak/mealient/App.kt
+++ app/src/debug/kotlin/gq/kirmanak/mealient/App.kt
@@ -1,33 +1,15 @@
 package gq.kirmanak.mealient
 
 import android.app.Application
-import com.facebook.flipper.android.AndroidFlipperClient
-import com.facebook.flipper.android.utils.FlipperUtils
-import com.facebook.flipper.core.FlipperPlugin
-import com.facebook.soloader.SoLoader
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
-import javax.inject.Inject
 
 @HiltAndroidApp
 class App : Application() {
-    // Use @JvmSuppressWildcards because otherwise dagger can't inject it (https://stackoverflow.com/a/43149382)
-    @Inject
-    lateinit var flipperPlugins: Set<@JvmSuppressWildcards FlipperPlugin>
 
     override fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
         Timber.v("onCreate() called")
-        setupFlipper()
-    }
-
-    private fun setupFlipper() {
-        if (FlipperUtils.shouldEnableFlipper(this)) {
-            SoLoader.init(this, false)
-            val flipperClient = AndroidFlipperClient.getInstance(this)
-            for (flipperPlugin in flipperPlugins) flipperClient.addPlugin(flipperPlugin)
-            flipperClient.start()
-        }
     }
 }
--- app/src/debug/kotlin/gq/kirmanak/mealient/di/DebugModule.kt
+++ app/src/debug/kotlin/gq/kirmanak/mealient/di/DebugModule.kt
@@ -1,23 +1,11 @@
 package gq.kirmanak.mealient.di
 
-import android.content.Context
-import com.facebook.flipper.core.FlipperPlugin
-import com.facebook.flipper.plugins.databases.DatabasesFlipperPlugin
-import com.facebook.flipper.plugins.inspector.DescriptorMapping
-import com.facebook.flipper.plugins.inspector.InspectorFlipperPlugin
-import com.facebook.flipper.plugins.leakcanary2.FlipperLeakListener
-import com.facebook.flipper.plugins.leakcanary2.LeakCanary2FlipperPlugin
-import com.facebook.flipper.plugins.network.FlipperOkhttpInterceptor
-import com.facebook.flipper.plugins.network.NetworkFlipperPlugin
-import com.facebook.flipper.plugins.sharedpreferences.SharedPreferencesFlipperPlugin
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import dagger.multibindings.IntoSet
 import gq.kirmanak.mealient.BuildConfig
-import leakcanary.LeakCanary
 import okhttp3.Interceptor
 import okhttp3.logging.HttpLoggingInterceptor
 import timber.log.Timber
@@ -26,6 +14,7 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 object DebugModule {
+
     @Provides
     @Singleton
     @IntoSet
@@ -37,46 +26,4 @@ object DebugModule {
         }
         return interceptor
     }
-
-    @Provides
-    @Singleton
-    @IntoSet
-    fun provideFlipperInterceptor(networkFlipperPlugin: NetworkFlipperPlugin): Interceptor {
-        return FlipperOkhttpInterceptor(networkFlipperPlugin)
-    }
-
-    @Provides
-    @Singleton
-    fun networkFlipperPlugin() = NetworkFlipperPlugin()
-
-    @Provides
-    @Singleton
-    @IntoSet
-    fun bindNetworkFlipperPlugin(plugin: NetworkFlipperPlugin): FlipperPlugin = plugin
-
-    @Provides
-    @Singleton
-    @IntoSet
-    fun sharedPreferencesPlugin(@ApplicationContext context: Context): FlipperPlugin =
-        SharedPreferencesFlipperPlugin(context)
-
-    @Provides
-    @Singleton
-    @IntoSet
-    fun leakCanaryPlugin(): FlipperPlugin {
-        LeakCanary.config = LeakCanary.config.copy(onHeapAnalyzedListener = FlipperLeakListener())
-        return LeakCanary2FlipperPlugin()
-    }
-
-    @Provides
-    @Singleton
-    @IntoSet
-    fun databasesPlugin(@ApplicationContext context: Context): FlipperPlugin =
-        DatabasesFlipperPlugin(context)
-
-    @Provides
-    @Singleton
-    @IntoSet
-    fun inspectorPlugin(@ApplicationContext context: Context): FlipperPlugin =
-        InspectorFlipperPlugin(context, DescriptorMapping.withDefaults())
 }
