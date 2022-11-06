0c0248dbbc1774c8a98a56c8ecfc72b574956f93
Kirill Kamakin <k.kamakin@protonmail.ch>
2022-11-06 20:02:51 +0100

    Ignore ingredients with empty note

--- app/src/main/java/gq/kirmanak/mealient/ui/recipes/info/RecipeInfoUiState.kt
+++ app/src/main/java/gq/kirmanak/mealient/ui/recipes/info/RecipeInfoUiState.kt
@@ -1,9 +1,15 @@
 package gq.kirmanak.mealient.ui.recipes.info
 
-import gq.kirmanak.mealient.database.recipe.entity.FullRecipeEntity
+import gq.kirmanak.mealient.database.recipe.entity.RecipeIngredientEntity
+import gq.kirmanak.mealient.database.recipe.entity.RecipeInstructionEntity
+import gq.kirmanak.mealient.database.recipe.entity.RecipeSummaryEntity
 
 data class RecipeInfoUiState(
-    val areIngredientsVisible: Boolean = false,
-    val areInstructionsVisible: Boolean = false,
-    val recipeInfo: FullRecipeEntity? = null,
+    val showIngredients: Boolean = false,
+    val showInstructions: Boolean = false,
+    val summaryEntity: RecipeSummaryEntity? = null,
+    val recipeIngredients: List<RecipeIngredientEntity> = emptyList(),
+    val recipeInstructions: List<RecipeInstructionEntity> = emptyList(),
+    val title: String? = null,
+    val description: String? = null,
 )
