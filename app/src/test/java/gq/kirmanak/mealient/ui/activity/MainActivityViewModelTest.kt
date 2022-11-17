4692c138d54203add9e5f86c053cbd85a3c0879b
Kirill Kamakin <k.kamakin@protonmail.ch>
2022-11-17 22:24:31 +0100

    Fix failing test

--- app/src/test/java/gq/kirmanak/mealient/ui/activity/MainActivityViewModelTest.kt
+++ app/src/test/java/gq/kirmanak/mealient/ui/activity/MainActivityViewModelTest.kt
@@ -4,7 +4,9 @@ import gq.kirmanak.mealient.data.auth.AuthRepo
 import gq.kirmanak.mealient.data.baseurl.ServerInfoRepo
 import gq.kirmanak.mealient.data.disclaimer.DisclaimerStorage
 import gq.kirmanak.mealient.data.recipes.RecipeRepo
+import gq.kirmanak.mealient.test.AuthImplTestData.TEST_BASE_URL
 import gq.kirmanak.mealient.test.BaseUnitTest
+import io.mockk.coEvery
 import io.mockk.every
 import io.mockk.impl.annotations.MockK
 import io.mockk.verify
@@ -32,6 +34,8 @@ class MainActivityViewModelTest : BaseUnitTest() {
     override fun setUp() {
         super.setUp()
         every { authRepo.isAuthorizedFlow } returns emptyFlow()
+        coEvery { disclaimerStorage.isDisclaimerAccepted() } returns true
+        coEvery { serverInfoRepo.getUrl() } returns TEST_BASE_URL
         subject = MainActivityViewModel(
             authRepo = authRepo,
             logger = logger,
@@ -49,6 +53,7 @@ class MainActivityViewModelTest : BaseUnitTest() {
 
     @Test
     fun `when onSearchQuery with null expect call to recipe repo`() {
+        subject.onSearchQuery("query")
         subject.onSearchQuery(null)
         verify { recipeRepo.updateNameQuery(null) }
     }
