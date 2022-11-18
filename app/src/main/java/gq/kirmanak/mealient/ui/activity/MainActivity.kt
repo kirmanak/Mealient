1c0d98850d3645e564142904608e42fd899d41bc
Kirill Kamakin <k.kamakin@protonmail.ch>
2022-11-18 22:53:08 +0100

    Disable action bar in theme

--- app/src/main/java/gq/kirmanak/mealient/ui/activity/MainActivity.kt
+++ app/src/main/java/gq/kirmanak/mealient/ui/activity/MainActivity.kt
@@ -44,7 +44,6 @@ class MainActivity : AppCompatActivity(R.layout.main_activity) {
         configureNavGraph()
         viewModel.uiStateLive.observe(this, ::onUiStateChange)
         binding.navigationView.setNavigationItemSelectedListener(::onNavigationItemSelected)
-        supportActionBar?.hide()
     }
 
     private fun configureNavGraph() {
