a202d6f0e23e19a68c089e6c4101f6d5017263b8
Kirill Kamakin <k.kamakin@protonmail.ch>
2022-12-22 22:07:56 +0100

    Remove unused fields from shopping lists response

--- datasource/src/main/kotlin/gq/kirmanak/mealient/datasource/v1/models/GetShoppingListsSummaryResponseV1.kt
+++ datasource/src/main/kotlin/gq/kirmanak/mealient/datasource/v1/models/GetShoppingListsSummaryResponseV1.kt
@@ -1,14 +1,10 @@
 package gq.kirmanak.mealient.datasource.v1.models
 
-import kotlinx.datetime.Instant
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class GetShoppingListsSummaryResponseV1(
     @SerialName("id") val id: String,
-    @SerialName("groupId") val groupId: String,
-    @SerialName("createdAt") val createdAt: Instant?,
-    @SerialName("updateAt") val updateAt: Instant?,
     @SerialName("name") val name: String?,
 )
