74020e3aa0335e555e691e70306c5e3591be297e
Kirill Kamakin <k.kamakin@protonmail.ch>
2022-12-22 20:46:12 +0100

    Add icon to shopping list name

--- features/shopping_lists/src/main/kotlin/gq/kirmanak/mealient/shopping_lists/ui/ShoppingListsFragment.kt
+++ features/shopping_lists/src/main/kotlin/gq/kirmanak/mealient/shopping_lists/ui/ShoppingListsFragment.kt
@@ -4,15 +4,24 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
 import dagger.hilt.android.AndroidEntryPoint
+import gq.kirmanak.mealient.shopping_list.R
 import gq.kirmanak.mealient.shopping_lists.network.ShoppingListInfo
 
 @AndroidEntryPoint
@@ -57,5 +66,14 @@ fun PreviewShoppingListCard() {
 
 @Composable
 fun ShoppingListCard(shoppingListInfo: ShoppingListInfo) {
-    Text(text = shoppingListInfo.name)
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Icon(
+            painter = painterResource(id = R.drawable.ic_shopping_cart),
+            contentDescription = stringResource(id = R.string.shopping_lists_screen_cart_icon),
+            modifier = Modifier.height(24.dp),
+        )
+        Text(text = shoppingListInfo.name)
+    }
 }
