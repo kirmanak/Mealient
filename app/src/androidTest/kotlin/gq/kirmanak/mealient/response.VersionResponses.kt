aeab503b8fb5e564c7ac8ac988a895c22dcbe24e
Kirill Kamakin <k.kamakin@protonmail.ch>
2023-11-04 11:08:06 +0100

    Fix first set up test

--- app/src/androidTest/kotlin/gq/kirmanak/mealient/response.VersionResponses.kt
+++ app/src/androidTest/kotlin/gq/kirmanak/mealient/response.VersionResponses.kt
@@ -5,11 +5,15 @@ import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
 import okhttp3.mockwebserver.RecordedRequest
 
-val versionV1Response = MockResponse().setResponseCode(200).setBody(
-    """{"production":true,"version":"v1.0.0beta-5","demoStatus":false,"allowSignup":true}"""
-)
+val versionV1Response = MockResponse()
+    .setResponseCode(200)
+    .setHeader("Content-Type", "application/json")
+    .setBody("""{"production":true,"version":"v1.0.0beta-5","demoStatus":false,"allowSignup":true}""")
 
-val notFoundResponse = MockResponse().setResponseCode(404).setBody("""{"detail":"Not found"}"""")
+val notFoundResponse = MockResponse()
+    .setResponseCode(404)
+    .setHeader("Content-Type", "application/json")
+    .setBody("""{"detail":"Not found"}"""")
 
 fun MockWebServer.dispatch(block: (String, RecordedRequest) -> MockResponse) {
     dispatcher = object : Dispatcher() {
