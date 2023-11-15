89a827150490c291b19d6eed2a1c41644296174d
Kirill Kamakin <k.kamakin@protonmail.ch>
2023-11-15 19:20:05 +0100

    Prepare v0.4.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ plugins {
 android {
     defaultConfig {
         applicationId = "gq.kirmanak.mealient"
-        versionCode = 32
-        versionName = "0.4.3"
+        versionCode = 33
+        versionName = "0.4.4"
         testInstrumentationRunner = "gq.kirmanak.mealient.MealientTestRunner"
         testInstrumentationRunnerArguments += mapOf("clearPackageData" to "true")
         resourceConfigurations += listOf("en", "es", "ru", "fr", "nl", "pt", "de")
