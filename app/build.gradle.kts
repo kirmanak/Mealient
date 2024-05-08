8ff3253923ceb81998ff4f1941f2866a6a130ffe
Kirill Kamakin <k.kamakin@protonmail.ch>
2024-05-08 20:30:55 +0200

    Use device and syntax from now in android app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 @file:Suppress("UnstableApiUsage")
 
+import com.android.build.api.dsl.ManagedVirtualDevice
 import java.io.FileInputStream
 import java.util.Properties
 
@@ -63,10 +64,10 @@ android {
 
     testOptions {
         managedDevices {
-            localDevices {
-                create("mediumPhoneApi34") {
-                    device = "Medium Phone"
-                    apiLevel = 34
+            devices {
+                maybeCreate("pixel4Api30", ManagedVirtualDevice::class.java).apply {
+                    device = "Pixel 4"
+                    apiLevel = 30
                     systemImageSource = "aosp-atd"
                 }
             }
