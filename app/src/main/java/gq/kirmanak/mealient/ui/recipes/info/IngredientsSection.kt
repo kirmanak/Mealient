9739659891d92a4335ac2c03e029964fea27c95c
Kirill Kamakin <k.kamakin@protonmail.ch>
2024-07-27 16:11:14 +0200

    Remove unused imports

--- app/src/main/java/gq/kirmanak/mealient/ui/recipes/info/IngredientsSection.kt
+++ app/src/main/java/gq/kirmanak/mealient/ui/recipes/info/IngredientsSection.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
 import androidx.compose.material3.Checkbox
-import androidx.compose.material3.Divider
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
