kirmanak/Mealient

commit 5dff173b6124b4e115ef189cccd9a856e4beda8d

Ensure editor displays correctly when switching to food mode.
- Fixed a bug causing the editor to display incorrectly when switching to food mode while at the bottom of the screen

features/shopping_lists/src/main/kotlin/gq/kirmanak/mealient/shopping_lists/ui/details/ShoppingListScreen.kt
@@ -240,6 +240,7 @@ fun ShoppingListItemEditor(
     showAddButton: (Boolean) -> Unit,
 ) {
     val bringIntoViewRequester = remember { BringIntoViewRequester() }
+    var shouldBringIntoView by remember { mutableStateOf(false) }
 
     Column(
         modifier = modifier
@@ -255,16 +256,18 @@ fun ShoppingListItemEditor(
         ShoppingListItemEditorButtonRow(
             state = state,
             onEditCancelled = onEditCancelled,
-            onEditConfirmed = onEditConfirmed
+            onEditConfirmed = onEditConfirmed,
+            // Bring editor back into view when the user switches between food and non-food items
+            onIconButtonClick = { shouldBringIntoView = true }
         )
     }
 
 
-    LaunchedEffect(Unit) {
-        // Hide the add button when the editor is active
-        showAddButton(false)
-        // Scroll to the editor when it's shown
-        bringIntoViewRequester.bringIntoView()
+    LaunchedEffect(shouldBringIntoView) {
+        if (shouldBringIntoView) {
+            bringIntoViewRequester.bringIntoView()
+            shouldBringIntoView = false
+        }
     }
     DisposableEffect(Unit) {
         onDispose {
@@ -337,6 +340,7 @@ private fun ShoppingListItemEditorButtonRow(
     modifier: Modifier = Modifier,
     onEditCancelled: () -> Unit = {},
     onEditConfirmed: () -> Unit = {},
+    onIconButtonClick: () -> Unit,
 ) {
     Row(
         modifier = modifier,
@@ -344,6 +348,7 @@ private fun ShoppingListItemEditorButtonRow(
     ) {
         IconButton(onClick = {
             state.isFood = !state.isFood
+            onIconButtonClick()
         }) {
             val stringId = if (state.isFood) {
                 R.string.shopping_list_screen_editor_not_food_button

commit d567dadd7e10984e0b793fb362106e15c08a8bf2

Fix regressions caused in 5dff173
- Fixed a regression introducing incorrect scrolling behaviour when editing items at the bottom of the page
- Fixed a regression causing the add button to not be hidden

features/shopping_lists/src/main/kotlin/gq/kirmanak/mealient/shopping_lists/ui/details/ShoppingListScreen.kt
@@ -263,11 +263,10 @@ fun ShoppingListItemEditor(
     }
 
 
-    LaunchedEffect(shouldBringIntoView) {
-        if (shouldBringIntoView) {
-            bringIntoViewRequester.bringIntoView()
-            shouldBringIntoView = false
-        }
+    LaunchedEffect (Unit, shouldBringIntoView) {
+        showAddButton(false)
+        bringIntoViewRequester.bringIntoView()
+        shouldBringIntoView = false
     }
     DisposableEffect(Unit) {
         onDispose {